0f9aab5fc438cb3f1f7a7ebe1f5cf78107793c75
fransbachtiar24 <fransbachtiar5@mhs.mdp.ac.id>
2022-12-01 16:35:27 +0700

    coba deploy api jadi public

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const mahasiswaRoutes = require("./routes/mahasiswa");
+require("dotenv/config");
+
+// middleware
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use("/mahasiswa", mahasiswaRoutes);
+
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+let db = mongoose.connection;
+
+// handle error
+db.on(
+  "error",
+  console.error.bind(console, "Error Establishing a Database Connection?")
+);
+// handle success
+db.once("open", () => {
+  console.log("Database is connected");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Running On ${process.env.PORT}`);
+});
---
+++ routes/mahasiswa.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const Mahasiswa = require("../models/Mahasiswa");
+
+// create (post)
+router.post("/", async (req, res) => {
+  const mahasiswaPost = new Mahasiswa({
+    nama: req.body.nama,
+    alamat: req.body.alamat,
+  });
+  try {
+    const mahasiswa = await mahasiswaPost.save();
+    // res.json(mahasiswa);
+    res.status(201).json(mahasiswa);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// read (get)
+router.get("/", async (req, res) => {
+  try {
+    const mahasiswa = await Mahasiswa.find();
+    res.json(mahasiswa);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:mahasiswaId", async (req, res) => {
+  try {
+    const mahasiswa = await Mahasiswa.findById(req.params.mahasiswaId);
+    res.json(mahasiswa);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+router.put("/:mahasiswaId", async (req, res) => {
+  // tampung inputan untuk value yang akan di edit
+
+  const data = {
+    nama: req.body.nama,
+    alamat: req.body.alamat,
+  };
+
+  try {
+    const mahasiswa = await Mahasiswa.updateOne(
+      { _id: req.params.mahasiswaId },
+      data
+    );
+    // response
+    res.status(200).json(mahasiswa);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// delete
+router.delete("/:mahasiswaId", async (req, res) => {
+  try {
+    const mahasiswa = await Mahasiswa.deleteOne({
+      _id: req.params.mahasiswaId,
+    });
+    res.status(200).json(mahasiswa);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
